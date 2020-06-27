3d428baf100203ca388e0ba0e1605503b4942bf6
zy <496351695@qq.com>
2020-06-27 21:26:17 +0800

    626提交测试

--- src/main/java/com/sfwl/bh/config/CorsConfig.java
+++ src/main/java/com/sfwl/bh/config/CorsConfig.java
@@ -15,5 +15,6 @@ public class CorsConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*").maxAge(-1);
+        //git 626提交测试
     }
 }
